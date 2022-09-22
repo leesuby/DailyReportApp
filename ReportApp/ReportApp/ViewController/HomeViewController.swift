cb9840b4eee0b376398860f2296e110f23efe04c
lap15335-local <leesuby.tl@gmail.com>
2022-09-22 10:33:14 +0700

    Refactor Code by split viewcontroller -> view + viewcontroller

---
+++ ReportApp/ReportApp/ViewController/HomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  ReportApp
+//
+//  Created by LAP15335 on 22/09/2022.
+//
+
+import Foundation
+
+class HomeViewController: ViewController{
+    
+}
---
+++ ReportApp/ReportApp/ViewController/LoginViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  ReportApp
+//
+//  Created by LAP15335 on 15/09/2022.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ViewController: UIViewController, LoginViewDelegate {
+    
+    
+    private var isInitial : Bool = true
+    
+    private var loginView = LoginView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginView.initialFirstLook(view: view)
+        loginView.delegate = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        if (isInitial){
+            loginView.initialLoginBox(view: view)
+            isInitial = false
+        }
+    }
+    
+   
+    
+    func btnLoginTapped(sender: UIButton) {
+        let button: UIButton = sender
+        if (button.tag == 1){
+            let email = loginView.emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = loginView.passTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            Auth.auth().signIn(withEmail: email, password: password){
+                (result,error) in
+                
+                if error != nil{
+                    print(error!.localizedDescription)
+                }
+                else {
+                    print("yeahhhh")
+                }
+                
+            }
+        }
+    }
+    
+    
+}
+
